15dc7c2e0db498ff76e08de594d85b09c5d725fc
hexagonix <felipemiguel_nery@hotmail.com>
2021-12-12 21:35:16 -0300

    Upload inicial das fontes

---
+++ fontes.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#;;************************************************************************************
+#;;
+#;;    
+#;;        %#@@%&@@%&@@%@             Sistema Operacional Andromeda®
+#;;        #@@@@@@#@#@#@@
+#;;        @#@@%    %#@#%
+#;;        @#@@@    #@#@@
+#;;        #@#@@    !@#@#     Copyright © 2016-2021 Felipe Miguel Nery Lunkes
+#;;        @#@%!@&%@&@#@#             Todos os direitos reservados
+#;;        !@@%#%&#&@&@%#
+#;;        @@#!%&@&@#&*@&
+#;;        @#@#%    &%@#@
+#;;        @#!@@    !#@#@                    Script versão 0.8
+#;;
+#;;
+#;;************************************************************************************
+
+echo
+echo "Construindo fontes do Hexagon®... {"
+echo
+
+for i in *.asm
+do
+
+	echo -n Gerando arquivo de fonte Hexagon® $(basename $i .asm)...
+	
+	fasm $i `basename $i .asm`.fnt >> /dev/null  || desmontar
+	
+	echo " [Ok]"
+	
+done
+
+echo
+echo "} Fontes geradas com sucesso."
