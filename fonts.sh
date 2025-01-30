0e8395e32e130d8a9e7ad803c3d182e51dff7c70
Felipe Lunkes <felipenldev@gmail.com>
2025-01-30 00:39:33 -0300

    Copyright update

--- fonts.sh
+++ fonts.sh
@@ -15,7 +15,7 @@
 #
 #                    Sistema Operacional Hexagonix - Hexagonix Operating System
 #
-#                         Copyright (c) 2015-2024 Felipe Miguel Nery Lunkes
+#                         Copyright (c) 2015-2025 Felipe Miguel Nery Lunkes
 #                        Todos os direitos reservados - All rights reserved.
 #
 #*************************************************************************************************
@@ -38,7 +38,7 @@
 #
 # BSD 3-Clause License
 #
-# Copyright (c) 2015-2024, Felipe Miguel Nery Lunkes
+# Copyright (c) 2015-2025, Felipe Miguel Nery Lunkes
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -105,7 +105,7 @@ function showVersion() {
 
 echo "hx build module for fonts, version $FONTS_MOD_VERSION"
 echo
-echo -e "\e[0mCopyright (c) 2015-2024 Felipe Miguel Nery Lunkes\e[0m"
+echo -e "\e[0mCopyright (c) 2015-2025 Felipe Miguel Nery Lunkes\e[0m"
 echo -e "hx and hx modules are licensed under BSD-3-Clause and comes with no warranty."
 
 }
